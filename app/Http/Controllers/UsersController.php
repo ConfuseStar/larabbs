9ca2ce8fb90fa4a14c156b8807c734eb5c541f91
SZL <492335413@qq.com>
2019-01-14 03:26:33 +0000

    限制头像分辨率

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,8 +27,8 @@ class UsersController extends Controller
                 $data['avatar'] = $result['path'];
             }
         }
-        $user->update($request->all());
 
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
