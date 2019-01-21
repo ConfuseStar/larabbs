607eba7d8cbdfe7ac8772030f4778bb7adaf169d
SZL <492335413@qq.com>
2019-01-21 09:19:44 +0000

    翻译队列化

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -32,7 +32,7 @@ class TopicsController extends Controller
 
         return view('topics.show', compact('topic'));
     }
-    
+
     public function create(Topic $topic)
     {
         $categories = Category::all();
